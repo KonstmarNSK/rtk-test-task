8b9d3c3ffea2acc8e1aaecfc967cd3d944eb9e37
konstantin <konstmar@yandex.ru>
2023-04-20 19:33:32 +0700

    added pom structure

---
+++ linkservice/src/test/java/com/kostya/rtttesttask/linkservice/LinkserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostya.rtttesttask.linkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
